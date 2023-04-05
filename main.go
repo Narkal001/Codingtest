a3d0128ac6f3df5805e9bbe67fa207cb4c60cdc0
Narkal001 <129604631+Narkal001@users.noreply.github.com>
2023-04-05 11:07:38 -0400

    first commit

--- main.go
+++ main.go
@@ -9,7 +9,6 @@ import (
 	"time"
 )
 
-// Currency struct represents the data for a single currency
 type Currency struct {
 	ID           string `json:"id"`
 	FullName     string `json:"fullName"`
@@ -26,12 +25,10 @@ type Currency struct {
 	PercentChage string `json:"percentChange"`
 }
 
-// CurrencyData struct represents the data for all supported currencies
 type CurrencyData struct {
 	Currencies []Currency `json:"currencies"`
 }
 
-// MarketTicker represents the data for a single market ticker
 type MarketTicker struct {
 	Ask    string `json:"ask"`
 	Bid    string `json:"bid"`
@@ -42,20 +39,16 @@ type MarketTicker struct {
 	Volume string `json:"volume"`
 }
 
-// Markets struct represents the data for all supported markets
 type Markets struct {
 	Markets map[string]MarketTicker `json:"markets"`
 }
 
-// Config struct represents the configuration data for the microservice
 type Config struct {
 	Symbols []string `json:"symbols"`
 }
 
-// CurrencyHandler handles the GET /currency/{symbol} endpoint
 func CurrencyHandler(markets *Markets) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		// Get the requested symbol and validate it
 		symbol := r.URL.Query().Get("symbol")
 		if symbol == "" {
 			http.Error(w, "No symbol specified", http.StatusBadRequest)
@@ -66,10 +59,8 @@ func CurrencyHandler(markets *Markets) http.HandlerFunc {
 			return
 		}
 
-		// Get the market ticker for the requested symbol
 		marketTicker := markets.Markets[symbol]
 
-		// Create a Currency object with the data from the market ticker
 		currency := Currency{
 			ID:          symbol,
 			FullName:    symbol,
@@ -79,10 +70,9 @@ func CurrencyHandler(markets *Markets) http.HandlerFunc {
 			Open:        marketTicker.Open,
 			Low:         marketTicker.Low,
 			High:        marketTicker.High,
-			FeeCurrency: "BTC", // Hard-coded for now
+			FeeCurrency: "BTC",
 		}
 
-		// Convert the Currency object to JSON and write it to the response
 		js, err := json.Marshal(currency)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -93,10 +83,8 @@ func CurrencyHandler(markets *Markets) http.HandlerFunc {
 	}
 }
 
-// CurrenciesHandler handles the GET /currency/all endpoint
 func CurrenciesHandler(markets *Markets) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		// Create a slice of Currency objects from the market tickers
 		var currencies []Currency
 		for symbol, marketTicker := range markets.Markets {
 			currency := Currency{
@@ -108,17 +96,15 @@ func CurrenciesHandler(markets *Markets) http.HandlerFunc {
 				Open:        marketTicker.Open,
 				Low:         marketTicker.Low,
 				High:        marketTicker.High,
-				FeeCurrency: "BTC", // Hard-coded for now
+				FeeCurrency: "BTC",
 			}
 			currencies = append(currencies, currency)
 		}
 
-		// Create a CurrencyData object with the slice of Currency objects
 		currencyData := CurrencyData{
 			Currencies: currencies,
 		}
 
-		// Convert the CurrencyData object to JSON and write it to the response
 		js, err := json.Marshal(currencyData)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -129,18 +115,14 @@ func CurrenciesHandler(markets *Markets) http.HandlerFunc {
 	}
 }
 
-// getConfig loads the configuration data from the config file and returns a Config object
 func getConfig() (*Config, error) {
-	// Hard-coded config file for now
 	configFile := "config.json"
 
-	// Read the config file
 	bytes, err := ReadFile(configFile)
 	if err != nil {
 		return nil, err
 	}
 
-	// Parse the JSON config data into a Config object
 	var config Config
 	err = json.Unmarshal(bytes, &config)
 	if err != nil {
@@ -150,33 +132,26 @@ func getConfig() (*Config, error) {
 	return &config, nil
 }
 
-// getMarkets gets the latest market ticker data for all supported symbols and returns
-// a Markets object with the data
 func getMarkets(symbols []string) (*Markets, error) {
-	// Set the HitBTC API endpoint
 	endpoint := "https://api.hitbtc.com/api/2/public/ticker"
 
-	// Make an HTTP request to the API endpoint
 	resp, err := http.Get(endpoint)
 	if err != nil {
 		return nil, err
 	}
 	defer resp.Body.Close()
 
-	// Read the response body into a byte slice
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		return nil, err
 	}
 
-	// Unmarshal the JSON response into a map of MarketTicker objects
 	var marketTickers map[string]MarketTicker
 	err = json.Unmarshal(body, &marketTickers)
 	if err != nil {
 		return nil, err
 	}
 
-	// Create a Markets object with the MarketTicker map
 	markets := Markets{
 		Markets: marketTickers,
 	}
@@ -184,19 +159,13 @@ func getMarkets(symbols []string) (*Markets, error) {
 	return &markets, nil
 }
 
-// updateMarkets periodically updates the market ticker data for all supported symbols and
-// updates the Markets object with the new data
 func updateMarkets(markets *Markets, symbols []string) {
-	// Set the update interval to 10 seconds
 	updateInterval := 10 * time.Second
 
-	// Create a ticker to trigger the update at the specified interval
 	ticker := time.NewTicker(updateInterval)
 
-	// Use a WaitGroup to block until all goroutines are finished
 	var wg sync.WaitGroup
 
-	// Start a goroutine to update the markets on each tick of the ticker
 	done := make(chan bool)
 	go func() {
 		for {
@@ -215,7 +184,6 @@ func updateMarkets(markets *Markets, symbols []string) {
 		}
 	}()
 
-	// Wait for the ticker goroutine to finish and mark the WaitGroup as done
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
@@ -223,28 +191,23 @@ func updateMarkets(markets *Markets, symbols []string) {
 		<-done
 	}()
 
-	// Wait for all goroutines to finish
 	wg.Wait()
 }
 
 func main() {
-	// Load the configuration data and get the list of supported symbols
 	config, err := getConfig()
 	if err != nil {
 		log.Fatalf("Error loading configuration: %v\n", err)
 	}
 	symbols := config.Symbols
 
-	// Get the initial market ticker data
 	markets, err := getMarkets(symbols)
 	if err != nil {
 		log.Fatalf("Error getting initial markets: %v\n", err)
 	}
 
-	// Start the goroutine to update the market ticker data periodically
 	go updateMarkets(markets, symbols)
 
-	// Set up the HTTP server and handlers
 	http.HandleFunc("/currency", func(w http.ResponseWriter, r *http.Request) {
 		http.Redirect(w, r, "/currency/all", http.StatusMovedPermanently)
 	})
@@ -258,12 +221,10 @@ func main() {
 		}
 	})
 
-	// Start the server on port 8080
 	log.Println("Starting server...")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-// ReadFile reads a file with the specified name and returns its contents as a byte slice
 func ReadFile(filename string) ([]byte, error) {
 	bytes, err := ioutil.ReadFile(filename)
 	if err != nil {
